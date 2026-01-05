dbc53143e7e3ab82cfc3638f431358347f07a213
Jeisi Rosales <Jeisirosales2003@gmail.com>
2026-01-04 23:17:55 -0400

    conexion con PostgreSQL creada

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude as ExcludeDecorator } from 'class-transformer';
+
+@Entity({ name: 'users' }) // Nombre de la tabla en Postgres
+export class User {
+  @PrimaryGeneratedColumn({ name: 'user_id' })
+  user_id: number;
+
+  @Column({ name: 'user_name' })
+  user_name: string;
+
+  @Column({ name: 'user_mail', unique: true })
+  user_mail: string;
+
+  @Column({ name: 'user_password' })
+  @ExcludeDecorator()
+  user_password: string;
+}
